b7b86f7ab76e28a062cace05bb61bbbbf15e332a
LauJoeYing <joeying0712@gmail.com>
2022-05-03 17:03:18 +0800

    add_new_task

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,6 +2,56 @@
 #include <stdlib.h>
 #define BUFFER_SIZE 1000
 
+void readFile(FILE * fPtr);
+
+void main(){
+    FILE *user_fPtr;
+
+    char username[50];
+    printf("Please Enter Your Account Username:\t");
+    scanf("%s",&username);
+    char user_password[] = "niuniu071209";
+    char user_name[] = "Ang Niu Niu";
+    char user_contact_num[] = "+60 12 690 0655";
+    char user_date_of_birth[] = "25/12/2019";
+    char user_email[] = "angniuniu@gmail.com";
+    char userData[1000] = "";
+
+    strcat(userData, username);
+    strcat(userData, " | ");
+    strcat(userData, user_password);
+    strcat(userData, " | ");
+    strcat(userData, user_name);
+    strcat(userData, " | ");
+    strcat(userData, user_contact_num);
+    strcat(userData, " | ");
+    strcat(userData, user_date_of_birth);
+    strcat(userData, " | ");
+    strcat(userData, user_email);
+    
+    user_fPtr = fopen("users.txt", "a");
+    
+    // /* Input data to append from user */
+    // printf("\nEnter data to append: ");
+    // fflush(stdin);          // To clear extra white space characters in stdin
+    // fgets(dataToAppend, BUFFER_SIZE, stdin);
+
+    /* Append data to file */
+    fprintf(user_fPtr, "\n%s", userData); 
+
+    /* Reopen file in read mode to print file contents */
+    user_fPtr = freopen("users.txt", "r", user_fPtr);
+
+    /* Print file contents after appending string */
+    printf("\nSuccessfully appended data to file. \n");
+    printf("Changed file contents:\n\n");         
+    readFile(user_fPtr);
+
+    /* Done with file, hence close file. */
+    fclose(user_fPtr);
+};
+
+
 //EOF= End of File
 void readFile(FILE * f_user)
 {
@@ -13,40 +63,4 @@ void readFile(FILE * f_user)
         putchar(ch);
 
     } while (ch != EOF);
-}
-
-
-char main(){
-    char username,user_password,user_name,user_contact_num,user_email;
-    char user_details[6];
-    char dataToAppend[BUFFER_SIZE];
-    FILE*f_user;
-    user_details[1] = username;
-    user_details[2] = user_password;
-    user_details[3] = user_name;
-    user_details[5] = user_contact_num;
-    user_details[6] = user_email;
-
-    f_user = fopen("users.txt", "r");
-    
-    /* Input data to append from user */
-    printf("\nEnter data to append: ");
-    fflush(stdin);          // To clear extra white space characters in stdin
-    fgets(dataToAppend, BUFFER_SIZE, stdin);
-
-    /* Append data to file */
-    fputs(dataToAppend, f_user);
-
-    /* Reopen file in read mode to print file contents */
-    f_user = freopen(user_details, "r", f_user);
-
-    /* Print file contents after appending string */
-    printf("\nSuccessfully appended data to file. \n");
-    printf("Changed file contents:\n\n");
-    readFile(f_user);
-
-    /* Done with file, hence close file. */
-    fclose(f_user);
-
-    return 0;
-}
+};
