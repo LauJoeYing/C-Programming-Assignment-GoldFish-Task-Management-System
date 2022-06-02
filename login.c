5418b0efd211a9cef45c3d7687c31f2645d4a21a
LauJoeYing <joeying0712@gmail.com>
2022-06-02 21:10:12 +0800

    update register features

--- login.c
+++ login.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "date.h"
+#include "dateValidation.c"
+#include "menu.c"
+#include "fileHandling.c"
+
 
 int userlogin();
 
@@ -10,7 +15,7 @@ struct User_data {
     char password[20];
     char name[256];
     char contact_num[21];
-    char date_of_birth[11];
+    Date date_of_birth;
     char email[321];
 }user;
 
@@ -21,8 +26,12 @@ int registration ();
 int main (void)
 {
     int option;
-    printf("Welcome to Goldfish Task Management System : )");
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    printf("\n\n\tWelcome to Goldfish Task Management System!\n\n");
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
     printf("\n[ 1 ] - Register\n[ 2 ] - Login\n\n");
+    
+    printf("Please Enter Your Choice Number: ");
     scanf("%d",&option);
 
     switch (option){
@@ -49,7 +58,7 @@ int login ()
     } else{
     struct User_data user;
 
-    printf("\nPlease Enter your login credentials below\n\n");
+    printf("\nPlease Enter Your Login Credentials Below:\n\n");
     printf("Username:\t");
     // scanf and fgets 
     scanf("%s",username);
@@ -67,7 +76,7 @@ int login ()
             }
         else 
             {
-                printf("\nIncorrect Login Details\nPlease enter the correct credentials\n");
+                printf("\nIncorrect Login Details\nPlease enter the correct credentials!\n");
                 continue;
             }
         }
@@ -83,41 +92,87 @@ int login ()
 int registration()
 {
     char firstname[15];
-    FILE *filePointer;
-
-    filePointer=fopen("user.txt","w");
-    if (filePointer == NULL)
-    {
-        fputs("Error at opening File!", stderr);
-        exit(1);
-    }
-
-
+    char user_password[21];
     struct User_data user;
 
-    printf("\nWelcome to Goldfish Task Management System. \n\n");
-    printf("\nEnter Your Name:\n");
-    scanf("%s",user.name);
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    printf("\n\n\t\tWelcome to New User Registration Page!\n\n");
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    printf("\n--------------------------------------------------------------------------");
+    fflush(stdin);
+    printf("\nPlease Enter Your Name:\n");
+    scanf("%[^\n]s",user.name);
     
-    printf("\nEnter Username:\n");
-    scanf("%s",user.username);
+    printf("--------------------------------------------------------------------------");
+    fflush(stdin);
+    printf("\nPlease Enter Your New Username:\n");
+    scanf("%[^\n]s",user.username);
+    
+    printf("--------------------------------------------------------------------------");
     while (1){
-    printf("\nEnter Password (8-20 characters): \n");
-    scanf("%s",user.password);
-    if (strlen(user.password)<=8 || strlen(user.password>=20)){
-        printf("Invalid Password! Please Ensure Your New Password have 8-20 Characters");
+        fflush(stdin);
+        printf("\nPlease Enter Your New Password (8-20 characters):\n");
+        scanf("%[^\n]s",user.password);
+        if (strlen(user.password)<8 || strlen(user.password)>20){
+            printf("\nInvalid Password! Please Ensure Your New Password have 8-20 Characters");
+            continue;}
+        fflush(stdin);
+        printf("\nPlease Enter Your New Password Again:\n");
+        scanf("%[^\n]s",user_password); 
+
+        if (strcmp(user.password, user_password) != 0){
+            printf("\nInvalid Input! Please Ensure That You Have Entered the Same Password!");
+            continue;
+        }   
+        
+        printf("\nCorrect Password Format!\nCongrats! Your New Password has been Verified!");
+        break;    
     }
+    
+
+    printf("\n--------------------------------------------------------------------------");
+    while (1){
+        fflush(stdin);
+        printf("\nPlease Enter Your Contact Number (without'-'):");
+        scanf("%[^\n]s",user.contact_num);
+        if (checkIsNumber(user.contact_num)){
+            if (strlen(user.contact_num)<10 || strlen(user.contact_num)>11){
+                printf("\nInvalid Phone Number! Please Ensure Your Registered Phone Number have 10 or 11 digits!");
+                continue;
+            }   printf("\nCorrect Phone Number Format!\nCongrats!Your Phone Number has been Verified!");
+                break;
+        }   
     }
 
+    printf("\n--------------------------------------------------------------------------");
+    printf("\n\nDate of Birth-");
+    user.date_of_birth = dateValidation();
+    printf("Congrats! Your Date of Birth has been Verified!");
 
-    fwrite(&user,sizeof(user),1,filePointer);
-    fclose(filePointer);
+    printf("\n--------------------------------------------------------------------------");
+    while (1){
+        printf("\n\nPlease Enter Your Email:\n");
+        fflush(stdin);
+        scanf("%[^\n]s",user.email);
+        if (strstr(user.email,"@") && strstr(user.email,".com")){   //To Check the Occurance of '@' and '.com'
+            printf("Congrats! Your Email has been Verified!");
+            break;
+        }   
+        printf("Wrong Email Format! Please Try Again!");
+        continue;
+    }
 
-    printf("\nConfirming details...\n...\nWelcome, %s!\n\n",user.name);
+    printf("\n--------------------------------------------------------------------------\n");
+    printf("\nConfirming details...");
+    printf("\n\nWelcome, %s : D !\n\n",user.name);
+    FILE * fileWriter = checkFileExistence("user.txt","w",0);
+    fwrite(&user, sizeof(struct User_data), 1, fileWriter);
+    fclose(fileWriter);
     printf("\nRegistration Successful!\n");
-    printf("Press any key to continue...");
-        getchar();
-    system("CLS");
     login();
+ 
 }
 
+// int verify_password()
+// int verify_email()
+// int verify_
